e5fea0eca8a1ed45161a684b99c8cb10ae8d4d05
framupla <kidalo1337@gmail.com>
2024-08-20 19:08:52 +0200

    Added logic + img

--- client/src/components/ContestMatchingUrl/MatchingUrl.jsx
+++ client/src/components/ContestMatchingUrl/MatchingUrl.jsx
@@ -1,21 +1,69 @@
-import React from 'react';
-import styles from './MatchingUrl.css'
+import React, { useState } from 'react';
+import styles from './MatchingUrl.css';
 
 const MatchingUrl = () => {
-    return (
-        <>
-        <span className='urlContainer'>
-        <div className='urlText'>
-            Do you want a matching domain (.com URL) with your name?
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleClick = (buttonNumber) => {
+    setSelectedButton(buttonNumber);
+  };
+
+  return (
+    <>
+      <span className="urlContainer">
+        <div className="urlText">
+          Do you want a matching domain (.com URL) with your name?
         </div>
-        </span>
-        <span className='urlButtonsContainer'>
-        <div className='urlButton'>Yes but minor variations are allowed</div>
-        <div className='urlButton'>Yes The Domain should exactly match the name</div>
-        <div className='urlButton'>No I am only looking for a name, not a Domain</div>
-        </span>
-        </>
-    );
-}
+      </span>
+      <span className="urlButtonsContainer">
+        <div
+          className={`urlButton ${selectedButton === 1 ? 'selected' : ''}`}
+          onClick={() => handleClick(1)}
+        >
+          <img
+            className="urlImg"
+            src="https://cdn-icons-png.flaticon.com/512/403/403474.png"
+          />
+          <div className={`answer ${selectedButton === 1 ? 'selected' : ''}`}>
+            Yes
+          </div>
+          <span className="answerDescription">
+            but minor variations are allowed
+          </span>
+        </div>
+        <div
+          className={`urlButton ${selectedButton === 2 ? 'selected' : ''}`}
+          onClick={() => handleClick(2)}
+        >
+          <img
+            className="urlImg"
+            src="https://cdn-icons-png.flaticon.com/512/403/403474.png"
+          />
+          <div className={`answer ${selectedButton === 2 ? 'selected' : ''}`}>
+            Yes
+          </div>
+          <span className="answerDescription">
+            The Domain should exactly match the name
+          </span>
+        </div>
+        <div
+          className={`urlButton ${selectedButton === 3 ? 'selected' : ''}`}
+          onClick={() => handleClick(3)}
+        >
+          <img
+            className="urlImg"
+            src="https://cdn-icons-png.flaticon.com/512/403/403474.png"
+          />
+          <div className={`answer ${selectedButton === 3 ? 'selected' : ''}`}>
+            No
+          </div>
+          <span className="answerDescription">
+            I am only looking for a name, not a Domain
+          </span>
+        </div>
+      </span>
+    </>
+  );
+};
 
 export default MatchingUrl;
