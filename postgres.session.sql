bf92f05fece8a860b94fe77cfc951a21f9e28b6d
framupla <kidalo1337@gmail.com>
2024-08-30 16:49:45 +0200

    Task 10

--- postgres.session.sql
+++ postgres.session.sql
@@ -4,3 +4,17 @@ count(id) as "Count"
 ,role
 from "Users"
 group by role
+
+--task 10
+update "Users"
+set balance = balance+sm
+from 
+
+(select sum(prize)*0.1 sm
+,"userId"
+from "Contests"
+where status = 'finished' 
+and "createdAt"::date between '2024-08-25' and '2024-08-26'
+group by "userId") cs
+
+where "Users".id = cs."userId"
