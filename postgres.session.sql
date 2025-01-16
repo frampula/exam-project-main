01dce4d290aa14065ba769086eef297ba8e18b84
framupla <kidalo1337@gmail.com>
2025-01-16 19:09:20 +0100

    Refactored sql files

--- postgres.session.sql
+++ postgres.session.sql
@@ -1,32 +1,29 @@
 --task 9
-select
-count(id) as "Count"
-,role
-from "Users"
-group by role
+SELECT
+    COUNT(id) AS "count",
+    role
+FROM "users"
+GROUP BY role;
 
 --task 10
-update "Users"
-set balance = balance+sm
-from 
-
-(select sum(prize)*0.1 sm
-,"userId"
-from "Contests"
-where status = 'finished' 
-and "createdAt"::date between '2024-08-25' and '2024-08-26'
-group by "userId") cs
-
-where "Users".id = cs."userId"
+UPDATE "users"
+SET balance = balance + sm
+FROM 
+    (SELECT SUM(prize) * 0.1 AS sm,
+            "userid"
+     FROM "contests"
+     WHERE status = 'finished' 
+       AND "createdat"::date BETWEEN '2024-08-25' AND '2024-08-26'
+     GROUP BY "userid") cs
+WHERE "users".id = cs."userid";
 
 --task 11
-update "Users"
-set balance = balance+10
-from 
-
-(select id
-from "Users"
-where role = 'creator'
-order by rating desc
-limit 3) t
-where "Users".id = t.id
+UPDATE "users"
+SET balance = balance + 10
+FROM 
+    (SELECT id
+     FROM "users"
+     WHERE role = 'creator'
+     ORDER BY rating DESC
+     LIMIT 3) t
+WHERE "users".id = t.id;
